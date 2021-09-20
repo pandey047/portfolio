d507a8056d25b0c44e82666164f96c9066d410f2
umesh <pandey047@gmail.com>
2021-09-20 20:46:34 +0530

    after adding mysqldatabases

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3")),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfoliodb',
+        'USER':'root',
+        'PASSWORD':'umesh12345',
     }
 }
 
